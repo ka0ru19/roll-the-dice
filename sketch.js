07ce5ec9376d7eab8587873861e1c1920409423f
井上 航 <winoue4506@office.baycurrent.co.jp>
2020-07-14 16:50:28 +0900

    テキスト版のさいころを追加

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+let diceCount; // さいころの個数
 let squareSize;
 let squareSpan;
 let textWidthSize;
-let diceNumber = [1, 2, 3, 4 ,5, 6];
+let diceEyes = [1, 2, 3, 4 ,5, 6];
+let customDiceEyes = ['船1', '船2', '船3', '黄', '青', '緑'];
 let diceHistory = [];
 let currentResult = [];
+let elementMargin = 20;
+let updateButtonWidth = 80;
+let isMousePressedObject = false;
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
@@ -11,60 +16,73 @@ function setup() {
   squareSize = windowWidth/6;
   squareSpan = squareSize + windowWidth/10;
   textWidthSize = windowWidth/20;
+  diceCount=2;
   
   rectMode(CENTER);
   textAlign(CENTER);
-  background(240);
-  
   textSize(textWidthSize);
-  text('Press Screen to Roll the Dice!', windowWidth/2, windowHeight*1/4);
   
-  drawSquare(
-    windowWidth/2 - squareSpan/2,
-    windowHeight/2,
-    squareSize,
-    1
-  );
+//   input = createInput();
+//   input.position(elementMargin, windowHeight*3/4);
+//   input.size(windowWidth - elementMargin*3 - updateButtonWidth);
+//   input.mousePressed(setMousePressedTrue);
+//   input.mouseReleased(setMousePressedFalse);
+
+//   button = createButton('update');
+//   button.position(input.x + input.width + elementMargin, windowHeight*3/4);
+//   button.size(updateButtonWidth);
+//   button.mousePressed(setMousePressedTrue);
+//   button.mouseReleased(setMousePressedFalse);
   
-  drawSquare(
-    windowWidth/2 + squareSpan/2,
-    windowHeight/2,
-    squareSize,
-    6
-  );
 } 
 
 function draw() {
-  currentResult = [
-     random(diceNumber),
-     random(diceNumber)
-  ];
-  if (mouseIsPressed == true) {
-    drawSquare(
-      windowWidth/2 - squareSpan/2,
-      windowHeight/2,
-      squareSize,
-      currentResult[0]
-    );
-    drawSquare(
-      windowWidth/2 + squareSpan/2,
-      windowHeight/2,
-      squareSize,
-      currentResult[1]
-    );
+  background(240);
+  textSize(textWidthSize);
+  text('Press Screen to Roll the Dice!', windowWidth/2, windowHeight*1/4);
+  if (mouseIsPressed == true && !isMousePressedObject) {
+    var resultArray = [];
+    for (let i=0; i<diceCount; i++) {
+      resultArray.push(random(diceEyes));
+    }
+    resultArray.push(random(customDiceEyes)); // 最後の要素はテキスト版
+    currentResult = resultArray;
   }
+  
+  for (let i=0; i<diceCount; i++) {
+    drawSquare(
+      windowWidth/2 + squareSpan*(-diceCount/2 + 1/2 + i),
+      windowHeight/2,
+      squareSize,
+      currentResult[i]
+    )
+  }
+  
+  drawTextSquare(
+    windowWidth/2,
+    windowHeight/2 + 100,
+    squareSize,
+    currentResult[currentResult.length-1]
+  );
+}
+
+function setMousePressedTrue() {
+  isMousePressedObject = true;
+}
+
+function setMousePressedFalse() {
+  isMousePressedObject = false;
 }
 
 function mouseReleased() {
   if (currentResult.length > 0) {
     diceHistory.push(currentResult);
-    currentResult = [];
   }
 }
 
-function drawSquare(centerX, centerY, size, number) {
-  const botSize = size/5;
-  const squareCornerRound = botSize;
+function drawTextSquare(centerX, centerY, size, eyeText) {
+  const textWidthSize = size/2;
+  const squareCornerRound = size/5;
   fill(250, 250, 250);
   rect(
     centerX,
@@ -74,7 +92,23 @@ function drawSquare(centerX, centerY, size, number) {
     squareCornerRound
   );
   fill(0);
-  switch(number) {
+  textSize(textWidthSize);
+  text(eyeText, centerX, centerY+size*0.2);//0.2は微調整分
+}
+
+function drawSquare(centerX, centerY, size, eyes) {
+  const botSize = size/5;
+  const squareCornerRound = size/5;
+  fill(250, 250, 250);
+  rect(
+    centerX,
+    centerY,
+    size,
+    size,
+    squareCornerRound
+  );
+  fill(0);
+  switch(eyes) {
     case 1:
       circle(
         centerX,

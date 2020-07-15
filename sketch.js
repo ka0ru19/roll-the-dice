87419ac770ed19a62b92641fc8fce4b55e02d45e
井上 航 <winoue4506@office.baycurrent.co.jp>
2020-07-15 12:03:42 +0900

    さいころ２こでの増減を可能にした

--- sketch.js
+++ sketch.js
@@ -6,37 +6,106 @@ let diceEyes = [1, 2, 3, 4 ,5, 6];
 let customDiceEyes = ['船1', '船2', '船3', '黄', '青', '緑'];
 let diceHistory = [];
 let currentResult = [];
-let elementMargin = 20;
-let updateButtonWidth = 80;
+let elementMargin = 12;
+let buttonWidth = 60;
 let isMousePressedObject = false;
+let dice2text;
+let dice2InputArray = [];
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
-  
-  squareSize = windowWidth/6;
-  squareSpan = squareSize + windowWidth/10;
-  textWidthSize = windowWidth/20;
+ 
   diceCount=2;
   
   rectMode(CENTER);
   textAlign(CENTER);
   textSize(textWidthSize);
   
-//   input = createInput();
-//   input.position(elementMargin, windowHeight*3/4);
-//   input.size(windowWidth - elementMargin*3 - updateButtonWidth);
-//   input.mousePressed(setMousePressedTrue);
-//   input.mouseReleased(setMousePressedFalse);
-
-//   button = createButton('update');
-//   button.position(input.x + input.width + elementMargin, windowHeight*3/4);
-//   button.size(updateButtonWidth);
-//   button.mousePressed(setMousePressedTrue);
-//   button.mouseReleased(setMousePressedFalse);
+  const diceTextWidth = 48;
+  dice2text = createDiv('dice2');
+  dice2text.position(elementMargin, windowHeight*3/4);
+  dice2text.size(diceTextWidth);
+  dice2text.style('color', '#000');
+  dice2text.style('background-color',  '#FFF');
   
-} 
+  const inputWidth = 20;
+  const domLineHeight = 30;
+  for (let i=0; i<6; i++) { // TODO: window widthが小さい場合のレイアウトを作る
+    var input = createInput();
+    var positionX = dice2text.x + dice2text.width + elementMargin + (inputWidth + elementMargin)*i;
+    var positionY = windowHeight*3/4;
+    input.position(positionX, positionY);
+    input.size(inputWidth);
+    input.value(diceEyes[i]);
+    input.mousePressed(setMousePressedTrue);
+    input.mouseReleased(setMousePressedFalse);
+    dice2InputArray.push(input);
+  }
+  
+  positionY = windowHeight*3/4 + domLineHeight;
+  var dice2UpdateButton = createDomButton(
+    'update',
+    elementMargin*2,
+    positionY
+  );
+  dice2UpdateButton.mousePressed(dice2Update);
+  
+  var dice2ColorButton = createDomButton(
+    'color',
+    dice2UpdateButton.x + dice2UpdateButton.width + elementMargin,
+    positionY
+  );
+  
+  var dice2RemoveButton = createDomButton(
+    'remove',
+    dice2ColorButton.x + dice2ColorButton.width + elementMargin,
+    positionY
+  );
+  dice2RemoveButton.mousePressed(dice2Remove);
+}
+
+function dice2Update() {
+  setMousePressedTrue();
+  diceCount = 2;
+  let isInputEmpty = true;
+  // 入力値について判定する
+  for (let dice2Input of dice2InputArray) {
+    if (dice2Input.value()) {
+      isInputEmpty = false;
+      break;
+    }
+  }
+  if (isInputEmpty) {
+    for (let i=0; i<diceEyes.length; i++) {
+      dice2InputArray[i].value(diceEyes[i]);
+    }
+  }
+}
+
+function dice2Remove() {
+  setMousePressedTrue();
+  diceCount = 1;
+  dice2text.style('color', '#000');
+  dice2text.style('background-color',  '#FFF');
+  for (let dice2Input of dice2InputArray) {
+    dice2Input.value('');
+  }
+}
+
+function createDomButton(txt, x, y) {
+  var button = createButton(txt);
+  button.position(x, y);
+  button.size(buttonWidth);
+  button.mousePressed(setMousePressedTrue);
+  button.mouseReleased(setMousePressedFalse);
+  return button;
+}
 
 function draw() {
+  squareSize = windowWidth/6;
+  squareSpan = squareSize + windowWidth/10;
+  textWidthSize = windowWidth/20;
+  
   background(240);
   textSize(textWidthSize);
   text('Press Screen to Roll the Dice!', windowWidth/2, windowHeight*1/4);
@@ -49,21 +118,33 @@ function draw() {
     currentResult = resultArray;
   }
   
-  for (let i=0; i<diceCount; i++) {
-    drawSquare(
-      windowWidth/2 + squareSpan*(-diceCount/2 + 1/2 + i),
-      windowHeight/2,
+  if (diceCount <= 2){
+    for (let i=0; i<diceCount; i++) {
+      drawSquare(
+        windowWidth/2 + squareSpan*(-diceCount/2 + 1/2 + i),
+        windowHeight/2,
+        squareSize,
+        currentResult[i]
+      );
+    }  
+  }
+  if (diceCount > 2) {
+    for (let i=0; i<2; i++) {
+      drawSquare(
+        windowWidth/2 + squareSpan*(-diceCount/2 + 1/2 + i),
+        windowHeight/2,
+        squareSize,
+        currentResult[i]
+      );
+    }
+    drawTextSquare(
+      windowWidth/2,
+      windowHeight/2 + squareSpan,
       squareSize,
-      currentResult[i]
-    )
+      currentResult[currentResult.length-1]
+    );  
   }
   
-  drawTextSquare(
-    windowWidth/2,
-    windowHeight/2 + squareSpan,
-    squareSize,
-    currentResult[currentResult.length-1]
-  );
 }
 
 function setMousePressedTrue() {
